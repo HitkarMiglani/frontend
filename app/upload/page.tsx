1e1e4785814ad4affd9adfe6894a28aafaf3ff10
Hitkar Miglani <144084967+HitkarMiglani@users.noreply.github.com>
2024-10-02 22:15:38 +0530

    hh

---
+++ app/upload/page.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client'
+import MRIAlzDetection from "@/components/File";
+import { FileUpload } from "@/components/ui/FileUpload";
+// import {ChangeEvent, FormEvent } from "react";
+
+export default function UploadPage() {
+  return (
+    <div>
+      <MRIAlzDetection />
+      
+      <div className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-black dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-lg">
+      <FileUpload />
+    </div>
+      
+      {/* {error && <p style={{ color: "red" }}>{error}</p>}
+      {result && (
+        <div>
+          <h3>Prediction Result:</h3>
+          <pre>{JSON.stringify(result, null, 2)}</pre>
+        </div> */}
+      {/* )} */}
+    </div>
+  );
+};
+
+// export default UploadPage;
