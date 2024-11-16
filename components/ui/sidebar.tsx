11450b1abc69a6124e04c4de0dbc0934cadf9be3
Hitkar Miglani <144084967+HitkarMiglani@users.noreply.github.com>
2024-11-16 11:23:27 +0530

    Update sidebar.tsx

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -70,15 +70,6 @@ export const Sidebar = ({
   );
 };
 
-export const SidebarBody = (props: React.ComponentProps<typeof motion.div>) => {
-  return (
-    <>
-      <DesktopSidebar {...props} />
-      {/* <MobileSidebar {...(props as React.ComponentProps<"div">)} /> */}
-    </>
-  );
-};
-
 export const DesktopSidebar = ({
   className,
   children,
@@ -105,55 +96,66 @@ export const DesktopSidebar = ({
   );
 };
 
-export const MobileSidebar = ({
-  className,
-  children,
-  ...props
-}: React.ComponentProps<"div">) => {
-  const { open, setOpen } = useSidebar();
+
+export const SidebarBody = (props: React.ComponentProps<typeof motion.div>) => {
   return (
     <>
-      <div
-        className={cn(
-          "h-10 px-4 py-4 flex flex-row md:hidden  items-center justify-between bg-neutral-100 dark:bg-neutral-800 w-full"
-        )}
-        {...props}
-      >
-        <div className="flex justify-end z-20 w-full">
-          <IconMenu2
-            className="text-neutral-100 dark:text-neutral-200"
-            onClick={() => setOpen(!open)}
-          />
-        </div>
-        <AnimatePresence>
-          {open && (
-            <motion.div
-              initial={{ x: "-100%", opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              exit={{ x: "-100%", opacity: 0 }}
-              transition={{
-                duration: 0.3,
-                ease: "easeInOut",
-              }}
-              className={cn(
-                "fixed h-full w-full inset-0 bg-white dark:bg-neutral-900 p-10 z-[100] flex flex-col justify-between",
-                className
-              )}
-            >
-              <div
-                className="absolute right-10 top-10 z-50 text-neutral-100 dark:text-neutral-200"
-                onClick={() => setOpen(!open)}
-              >
-                <IconX />
-              </div>
-              {children}
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </div>
+      <DesktopSidebar {...props} />
+      {/* <MobileSidebar {...(props as React.ComponentProps<"div">)} /> */}
     </>
   );
 };
+
+
+// export const MobileSidebar = ({
+//   className,
+//   children,
+//   ...props
+// }: React.ComponentProps<"div">) => {
+//   const { open, setOpen } = useSidebar();
+//   return (
+//     <>
+//       <div
+//         className={cn(
+//           "h-10 px-4 py-4 flex flex-row md:hidden  items-center justify-between bg-neutral-100 dark:bg-neutral-800 w-full"
+//         )}
+//         {...props}
+//       >
+//         <div className="flex justify-end z-20 w-full">
+//           <IconMenu2
+//             className="text-neutral-100 dark:text-neutral-200"
+//             onClick={() => setOpen(!open)}
+//           />
+//         </div>
+//         <AnimatePresence>
+//           {open && (
+//             <motion.div
+//               initial={{ x: "-100%", opacity: 0 }}
+//               animate={{ x: 0, opacity: 1 }}
+//               exit={{ x: "-100%", opacity: 0 }}
+//               transition={{
+//                 duration: 0.3,
+//                 ease: "easeInOut",
+//               }}
+//               className={cn(
+//                 "fixed h-full w-full inset-0 bg-white dark:bg-neutral-900 p-10 z-[100] flex flex-col justify-between",
+//                 className
+//               )}
+//             >
+//               <div
+//                 className="absolute right-10 top-10 z-50 text-neutral-100 dark:text-neutral-200"
+//                 onClick={() => setOpen(!open)}
+//               >
+//                 <IconX />
+//               </div>
+//               {children}
+//             </motion.div>
+//           )}
+//         </AnimatePresence>
+//       </div>
+//     </>
+//   );
+// };
 
 export const SidebarLink = ({
   link,
