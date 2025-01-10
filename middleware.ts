335bca6eebf977c4b6b712c37186a9edd75db035
Hitkar Miglani <144084967+HitkarMiglani@users.noreply.github.com>
2025-01-10 13:26:52 +0530

    c

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
-import { clerkMiddleware,createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/Patients(.*)"]);
+// const isProtectedRoute = createRouteMatcher(["/Patients(.*)"]);
 
 export default clerkMiddleware();
 
